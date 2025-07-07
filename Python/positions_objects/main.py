771f6d667deea802e8360ce4096a0fef39f1ed06
Agata Kozioł <agata.koziol.98@gmail.com>
2025-07-07 17:49:49 +0200

    Cleaned main.py

--- Python/positions_objects/main.py
+++ Python/positions_objects/main.py
@@ -1,50 +1,38 @@
-import pandas as pd
-from matplotlib import pyplot as plt
 from plotting.plot_data import make_boxplots_interaction, make_boxplots_interaction_count
 from imports.import_data import extract_sampling_across_positions, extract_sampling_per_id, generateBodyDescriptives, \
     generateManualDescriptives
 
-timepoint = 'T3'
-extract_data = 1
-DIR = 'G:/Mój dysk/POSITION_TOY_9MO/SCRIPTS/clean_code/data/'
-OUT_DIR = 'G:/Mój dysk/POSITION_TOY_9MO/SCRIPTS/clean_code/plots/'
+DIR = ''
+OUT_DIR = ''
 
-if extract_data:
-    body = generateBodyDescriptives(DIR,timepoint)
-    manual = generateManualDescriptives(DIR, timepoint)
-    sampling_across_positions = extract_sampling_across_positions(DIR, timepoint, body, manual)
-    sampling_per_id = extract_sampling_per_id(DIR, sampling_across_positions[sampling_across_positions['Tier'] != 'mouthing'], timepoint)
+body = generateBodyDescriptives(DIR)
+manual = generateManualDescriptives(DIR)
+sampling_across_positions = extract_sampling_across_positions(DIR, body, manual)
+sampling_per_id = extract_sampling_per_id(DIR, aff_Object='Object', con_position='Position')
 
-# Load data
-else:
-    sampling_across_positions = pd.read_csv(DIR + timepoint + '/sampling_across_positions.csv', index_col=0)
-    sampling_per_id = pd.read_csv(DIR + timepoint + '/sampling_across_positions_counts.csv', index_col=0)
-
-sampling_across_positions = sampling_across_positions[sampling_across_positions['Tier'] != 'mouthing'].reset_index(drop=True)
 sampling_per_id = sampling_per_id[sampling_per_id['count'] > 0].reset_index(drop=True)
 
-df_sit = sampling_across_positions[sampling_across_positions['Position'] == 'Sitting']
+df_sit = sampling_across_positions[sampling_across_positions['Position'] == 'Independent sitting']
 df_non = sampling_across_positions[sampling_across_positions['Position'] != 'Sitting']
 
-sampling_per_id_sit = sampling_per_id[sampling_per_id['Condition'] == 'Independent sitting']
-sampling_per_id_non = sampling_per_id[sampling_per_id['Condition'] == 'Other']
+sampling_per_id_sit = sampling_per_id[sampling_per_id['Position'] == 'Independent sitting']
+sampling_per_id_non = sampling_per_id[sampling_per_id['Position'] == 'Other']
 
-df_sit_mdn = df_sit.groupby(['id', 'Affordances', 'Condition'])['Duration'].median().reset_index(drop=False)
-df_non_mdn = df_non.groupby(['id', 'Affordances', 'Condition'])['Duration'].median().reset_index(drop=False)
+df_sit_mdn = df_sit.groupby(['id', 'Object', 'Position'])['Duration'].median().reset_index(drop=False)
+df_non_mdn = df_non.groupby(['id', 'Object', 'Position'])['Duration'].median().reset_index(drop=False)
 
 make_boxplots_interaction(df_sit_mdn, df_non_mdn,
                           y_var='Duration',
                           toys=['graspable', 'stationary'],
                           colors={"graspable": "#2bc3db",
-                         "stationary": "#bfd739"},
+                                  "stationary": "#bfd739"},
                           toy_images={'graspable':
-                            {'dino_img': plt.imread(DIR + timepoint + '/images/trademark/dino.png'),
-                            'bubbles_img': plt.imread(DIR + timepoint + '/images/trademark/bubbles.png')},
-                          'stationary':
-                            {'klickity_img': plt.imread(DIR + timepoint + '/images/trademark/klickity.png'),
-                            'spinner_img': plt.imread(DIR + timepoint + '/images/trademark/spinner.png')}},
+                                            {'dino_img': 'PATH',
+                                            'bubbles_img': 'PATH'},
+                                       'stationary':
+                                            {'klickity_img': 'PATH',
+                                            'spinner_img': 'PATH'}},
                           fig_size=(10, 10),
-                          save_name=f"{OUT_DIR}/{timepoint}/boxplots_interaction_mdn.png",
                           )
 
 make_boxplots_interaction_count(sampling_per_id_sit, sampling_per_id_non,
@@ -53,13 +41,10 @@ make_boxplots_interaction_count(sampling_per_id_sit, sampling_per_id_non,
                           colors={"graspable": "#2bc3db",
                          "stationary": "#bfd739"},
                           toy_images={'graspable':
-                            {'dino_img': plt.imread(DIR + timepoint + '/images/trademark/dino.png'),
-                            'bubbles_img': plt.imread(DIR + timepoint + '/images/trademark/bubbles.png')},
-                          'stationary':
-                            {'klickity_img': plt.imread(DIR + timepoint + '/images/trademark/klickity.png'),
-                            'spinner_img': plt.imread(DIR + timepoint + '/images/trademark/spinner.png')}},
+                                            {'dino_img': 'PATH',
+                                             'bubbles_img': 'PATH'},
+                                        'stationary':
+                                            {'klickity_img': 'PATH',
+                                             'spinner_img': 'PATH'}},
                           fig_size=(10, 10),
-                          save_name=f"{OUT_DIR}{timepoint}/vboxplots_count_interaction.png",
                           )
-
-
